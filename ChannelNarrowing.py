c7ab962c6cab022f13618898f443822756032c86
banderson1618 <banderson1618@gmail.com>
2018-05-15 11:31:19 -0600

    Remove created polygons from segmenting process

--- ChannelNarrowing.py
+++ ChannelNarrowing.py
@@ -14,10 +14,7 @@ def main(historicBankfull, modernBankfull, reachBreak, outputFolder, outputName,
     :param isSegmented: Tells us if the technician has segmented the input beforehand
     :return:
     """
-    if isSegmented == 'false' or isSegmented is None:
-        isSegmented = False
-    else:
-        isSegmented = True
+    isSegmented = parseArcBool(isSegmented)
 
     if not isSegmented and not reachBreak:
         raise Exception("You did not provide a way for us to segment the inputs!")
@@ -27,6 +24,8 @@ def main(historicBankfull, modernBankfull, reachBreak, outputFolder, outputName,
     if not isSegmented:
         intermediaryFolder = makeFolder(outputFolder, "02_Intermediaries")
         historicBankfull, modernBankfull = segmentBankfull(historicBankfull, modernBankfull, reachBreak, intermediaryFolder)
+
+
 
 
 
@@ -43,14 +42,37 @@ def segmentBankfull(historicBankfull, modernBankfull, reachBreak, intermediaryFo
     segHistoricBankfullFolder = makeFolder(intermediaryFolder, "01_HistoricBankfullSegmented")
     segModernBankfullFolder = makeFolder(intermediaryFolder, "02_ModernBankfullSegmented")
 
+    historicBankfullLayer = os.path.join(segHistoricBankfullFolder, "old.lyr")
+    arcpy.MakeFeatureLayer_management(historicBankfull, historicBankfullLayer)
+    modernBankfullLayer = os.path.join(segModernBankfullFolder, "old.lyr")
+    arcpy.MakeFeatureLayer_management(modernBankfull, modernBankfullLayer)
+
     segHistoricBankfull = os.path.join(segHistoricBankfullFolder, "historicBankfull.shp")
     segModernBankfull = os.path.join(segModernBankfullFolder, "modernBankfull.shp")
 
+    clippedSegHistoricBankfull = os.path.join(segHistoricBankfullFolder, "BetterHistoric.shp")
+    clippedSegModernBankfull = os.path.join(segModernBankfullFolder, "BetterModern.shp")
+
     arcpy.Delete_management(segHistoricBankfull)
     arcpy.Delete_management(segModernBankfull)
+    arcpy.Delete_management(clippedSegHistoricBankfull)
+    arcpy.Delete_management(clippedSegModernBankfull)
+
+    segHistoricBankfullLayer = os.path.join(segHistoricBankfullFolder, "historicBankfull_lyr")
+    segModernBankfullLayer = os.path.join(segModernBankfullFolder, "modernBankfull_lyr")
 
     arcpy.FeatureToPolygon_management([historicBankfull, reachBreak], segHistoricBankfull)
+    arcpy.MakeFeatureLayer_management(segHistoricBankfull, segHistoricBankfullLayer)
     arcpy.FeatureToPolygon_management([modernBankfull, reachBreak], segModernBankfull)
+    arcpy.MakeFeatureLayer_management(segModernBankfull, segModernBankfullLayer)
+
+    cleanedSegHistoricBankfull = os.path.join(segHistoricBankfullFolder, "BetterHistoric.shp")
+    arcpy.SelectLayerByLocation_management(segHistoricBankfullLayer, "WITHIN", historicBankfullLayer)
+    arcpy.CopyFeatures_management(segHistoricBankfullLayer, cleanedSegHistoricBankfull)
+
+    cleanedSegModernBankfull = os.path.join(segModernBankfullFolder, "CleanedModernBankfull.shp")
+    arcpy.SelectLayerByLocation_management(segModernBankfullLayer, "WITHIN", modernBankfullLayer)
+    arcpy.CopyFeatures_management(segModernBankfullLayer, cleanedSegModernBankfull)
 
     return segHistoricBankfull, segModernBankfull
 
@@ -65,3 +87,15 @@ def makeFolder(pathToLocation, newFolderName):
     if not os.path.exists(newFolder):
         os.mkdir(newFolder)
     return newFolder
+
+
+def parseArcBool(givenBool):
+    """
+    Resolves the text given as a boolean
+    :param givenBool: The bool passed to the tool by the user
+    :return:
+    """
+    if givenBool == 'false' or givenBool is None:
+        return False
+    else:
+        return True
